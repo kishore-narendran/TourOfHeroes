0f58e1050a069c0bc229a00b35bc5ff1fb7daa06
Kishore Narendran <kishore.narendran09@gmail.com>
2017-01-19 18:05:18 -0800

    Adds the sample Angular2 application:

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,22 @@
-import { Component } from '@angular/core';
+/**
+ * Created by kishorenarendran on 1/17/17.
+ */
+import {Component} from "@angular/core";
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+      <h1>{{title}}</h1>
+      <nav>
+        <a routerLink="/dashboard">Dashboard</a>
+        <a routerLink="/heroes">Heroes</a>
+      </nav>
+      <router-outlet></router-outlet>
+  `,
+  styleUrls: ['app.component.css']
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
---
+++ app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by kishorenarendran on 1/17/17.
+ */
+export class Hero {
+  id: number;
+  name: string;
+}
